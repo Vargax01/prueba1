01aca8edc8f2579f7b8ab9de5e98573243d6b905
vargax <miguelchico14@gmail.com>
2017-03-05 21:57:49 +0100

    He creado y hecho el ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from lxml import etree
+doc = etree.parse('MozartPianoSonata.xml')
+raiz=doc.getroot()
+notas1=[]
+compas=str(raw_input("Dame número de compás: "))
+for elem in raiz.find("part"):
+    if elem.attrib["number"] == compas:
+        notas=elem.findall("note/pitch/step")
+        for nota in notas:
+            if nota.text not in notas1:
+                notas1.append(nota.text)
+
+print "Las notas que aparecén en el compás", compas, "son:"
+for nota1 in notas1:
+    if nota1 == "C":
+        print "do"
+    elif nota1 == "D":
+        print "re"
+    elif nota1 == "E":
+        print "mi"
+    elif nota1 == "F":
+        print "fa"
+    elif nota1 == "G":
+        print "sol"
+    elif nota1 == "A":
+        print "la"
+    else:
+        print "si"
