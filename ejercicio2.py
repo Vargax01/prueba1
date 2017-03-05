03a3030432ffce42ba044a09462e79d38eee4209
vargax <miguelchico14@gmail.com>
2017-03-05 22:44:35 +0100

    terminado ej2

--- ejercicio2.py
+++ ejercicio2.py
@@ -17,4 +17,14 @@ elif nota == "la":
     nota1="A"
 else:
     nota1="B"
-print nota1
+for elem in raiz.find("part"):
+    print "Compas", elem.attrib["number"], ":"
+    notas=elem.findall("note/pitch/step")
+    cont=0
+    for nota2 in notas:
+        if nota2.text == nota1:
+            cont=cont+1
+    if cont == 0:
+        print "La nota", nota, "no aparece en este compás"
+    else:
+        print "La nota", nota,"aparece", cont, "veces"
